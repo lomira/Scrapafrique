3a60242a2fd037c12c5bd633101b414c2b2f0a98
lomi <lomi@lomag.com>
2020-09-30 21:34:37 +0200

    Working simple

---
+++ Scrafrique/helpers.py
@@ -0,0 +1,6 @@
+import varname
+
+def print_neuf(lista, listb):
+    if lista==listb:
+        print("Ya du neuf :" + varname.nameof(lista))
+    
--- Scrafrique/scrapafrique.py
+++ Scrafrique/scrapafrique.py
@@ -1,11 +1,21 @@
 import scrappers
+import helpers
+import pickle
 
+pickle_path = "data/output.pickel"
+with open(pickle_path, "rb") as f:
+    niger_old, senegal_old = pickle.load(f)
 
+# niger_old = []
 niger = scrappers.niger("https://www.arcep.ne/publications.php?sid=96")
-# print(*niger, sep="\n")
+nouveaux = set(niger) - set(niger_old)
+if nouveaux:
+    print(*nouveaux, sep="\n")
 
 
-senegal = scrappers.senegal(
-    "https://www.artpsenegal.net/fr/espace-pro/appels-doffres"
-)
-print(*senegal, sep="\n")
+senegal = scrappers.senegal("https://www.artpsenegal.net/fr/espace-pro/appels-doffres")
+# print(*senegal, sep="\n")
+
+
+with open(pickle_path, "wb") as f:
+    pickle.dump([niger, senegal], f)
